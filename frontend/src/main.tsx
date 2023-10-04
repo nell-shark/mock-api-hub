6999efebd01d817be15130215dbbb1aef137fe3b
NellShark <krivolapov_vladislav@outlook.com>
2023-10-05 00:12:02 +0600

    Create ErrorBoundary

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,12 @@ import ReactDOM from 'react-dom/client';
 import { App } from '@/App';
 import '@/index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { ErrorBoundary } from '@components/ErrorBoundary';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ErrorBoundary>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ErrorBoundary>
 );
